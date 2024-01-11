d6d5cfb5b829c27451c5d136f3e812347865ab10
leo <nascimentoleo899@gmail.com>
2024-01-10 21:06:16 -0300

    chore: initial config

---
+++ src/main/java/com/leonardo/heaxagonal/HeaxagonalApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.heaxagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaxagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeaxagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/heaxagonal/HeaxagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.heaxagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeaxagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
